8c3b17e56708274fe3c839e2f15e3f4855818cf0
Muppidathi Boominathan <104143415+Muppidathi336@users.noreply.github.com>
2022-08-20 17:56:12 +0530

    Update ColoredShapeDaoImpl.java

--- ColoredShapeApplication/src/main/java/com/mb/ColoredShapeApplication/ColoredShapeDaoImpl.java
+++ ColoredShapeApplication/src/main/java/com/mb/ColoredShapeApplication/ColoredShapeDaoImpl.java
@@ -1,13 +1,13 @@
 package com.mb.ColoredShapeApplication;
 
+//below for hibernate alone(use session factory), the actual for hibernate and jpa(use EM)
+/*
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
-
 
 @Repository
 @Transactional(readOnly = true)
@@ -32,5 +32,38 @@ public class ColoredShapeDaoImpl implements ColoredShapeDao {
     public void save(final ColoredShape coloredShape) {
 
         getSession().saveOrUpdate(coloredShape);
+    }
+}
+*/
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Repository
+@Transactional(readOnly = true)
+public class ColoredShapeDaoImpl implements ColoredShapeDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+
+    public ColoredShape findById(final Long id) {
+
+        return (ColoredShape) em
+                .createQuery("SELECT c FROM ColoredShape c WHERE c.id LIKE :id")
+                .setParameter("id", id)
+                .setMaxResults(1)
+                .getResultList().get(0);
+    }
+
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRES_NEW)
+    public void save(final ColoredShape coloredShape) {
+
+        em.merge(coloredShape);
     }
 }
